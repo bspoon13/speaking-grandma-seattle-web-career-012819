95c27d1e3a919a0ea3659a691a33f96ec456e573
Billy Witherspoon <wwither@gmail.com>
2019-01-15 05:42:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elseif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
     "NO, NOT SINCE 1983"
